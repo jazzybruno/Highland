9e2274e7880fb08e49cfcc442cb8cfca36d187bd
jazzybruno <95282931+jazzybruno@users.noreply.github.com>
2022-07-29 11:48:55 +0200

    the about us page

--- highland/src/components/pages/AboutUs/Aboutus.jsx
+++ highland/src/components/pages/AboutUs/Aboutus.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import Header from "../../ui/Header/Header";
 import Footer from "../../ui/Footer/Footer";
+import Video from '../../videos/about.mp4';
+import './About.css'
 
 const AboutUs = () => {
     return (
@@ -9,11 +11,11 @@ const AboutUs = () => {
         {/* tthe about us page start */}
         <div className="aboutus">
          {/* the video secton  start */}
-         
+          <video src={Video} autoPlay loop muted />
          {/* the video secton  end */}
-        </div>
         {/* tthe about us page end */}
         <Footer />
+        </div>
         </div>
     );
 }
