17af9e6a3afe9208a5f0f5182c13ccf45215cdc0
IrisNgabo <98657891+IrisNgabo@users.noreply.github.com>
2022-08-09 13:01:26 +0200

    forms added

--- src/App.jsx
+++ src/App.jsx
@@ -6,13 +6,16 @@ import Admission from './components/pages/Admission/Admission';
 import {BrowserRouter , Route , Routes} from 'react-router-dom'
 
 
+
+
 const App = ()=>{
   return (
     <div>
  
       <BrowserRouter>
         <Routes> 
-        <Route path='/' element={<Landing />} />
+       
+        <Route path='/Landing' element={<Landing />} />
           <Route path="/about" element={<AboutUs/>} />
           <Route path="/admission" element={<Admission/>} />
           <Route path="/contact" element={<Contact/>} />
---
+++ src/components/ui/Registration/Registration.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import './Registration.css'
+
+function Registration() {
+  return (
+    <div className='registration-body'>
+        <h1 className='registration-head'>Registration fees</h1>
+        <p className='registration-para'>The registration fees are 10,000 Rwandan Francs ,and canbe payed either on bank or momo</p>
+        <p className='registration-para'>You will get a call no more than today confirming the day of interviewing the child.</p>
+        <h2>You should come with the following Documents:</h2>
+        <li>Student's prrevious report card for a usual student</li><br/>
+        <li>The birth certificate for baby beginners</li><br/>
+        <li>The confirmation that the registration fees were paid</li><br/>
+        <li>The student ready for the interview</li><br/>
+        <div className="finsih-registration">
+        <button className='registration-finish'>Finish Enrollment</button>
+        </div>
+    </div>
+
+  )
+}
+
+export default Registration
